98486e938dd217979105ccf95fb5bc9e1c0f187e
foben <f.obenauer@gmail.com>
2013-07-16 22:06:06 +0200

    proper writing

--- src/main/java/net/foben/schematizer/stats/LOVStat.java
+++ src/main/java/net/foben/schematizer/stats/LOVStat.java
@@ -1,13 +1,20 @@
 package net.foben.schematizer.stats;
 
+import java.io.IOException;
+import java.io.Writer;
 import java.util.HashMap;
 import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class LOVStat {
 	private String PLDString;
 	private Map<String, Integer> lovmap;
+	private Logger _log;
 	
 	public LOVStat(String pldString){
+		_log = LoggerFactory.getLogger(LOVStat.class);
 		this.PLDString = pldString;
 		lovmap = new HashMap<String, Integer>();
 	}
@@ -27,6 +34,19 @@ public class LOVStat {
 			result += PLDString + " " + type + " " + lovmap.get(type) + "\n";
 		}
 		return result;
+	}
+
+	public void write(Writer w) throws IOException {
+		int lines = 0;
+		for(String lov : lovmap.keySet()){
+			lines++;
+			w.write(PLDString + " " + lov + " " + lovmap.get(lov) + "\n");
+			if(lines%5000 == 0){
+				_log.info("Flushing 5000 lines");
+				w.flush();
+			}
+		}
+		
 	}
 
 	
