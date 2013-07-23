98f8c51ac90f2b2265ac548a5ad9b2fbfb4e6fc5
foben <f.obenauer@gmail.com>
2013-07-23 14:59:27 +0200

    abstractify!

---
+++ src/main/java/net/foben/schematizer/parse/BenchmarkingHandler.java
@@ -0,0 +1,39 @@
+package net.foben.schematizer.parse;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.openrdf.model.Statement;
+import org.openrdf.rio.RDFHandlerException;
+
+public class BenchmarkingHandler extends AbstractHandler {
+	
+	long timeten;
+	Map<Integer, Double> avgs;
+	
+	public BenchmarkingHandler(){
+		super();
+		timeten = System.nanoTime();
+		avgs = new HashMap<Integer, Double>();
+	}
+	
+	@Override
+	public void handleStatementInternal(Statement st) {
+		
+		if(stcount%10000000 == 0){
+			double avgten = ((System.nanoTime()-timeten) / 10d) / 1000000000d;
+			_log.info("   Average last 10 million: " + avgten);
+			avgs.put(stcount/10000000, avgten);
+			timeten = System.nanoTime();
+		}
+
+	}
+	
+	@Override
+	public void endRDF() throws RDFHandlerException {
+		_log.info("Averages");
+		for(Integer key : avgs.keySet()){
+			_log.info(" " + key + " : " + avgs.get(key));
+		}
+	}
+}
