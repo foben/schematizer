0fb0b629978eabb82f948df130af029ab32a30a0
foben <f.obenauer@gmail.com>
2013-07-28 17:08:04 +0200

    yo

--- src/main/java/net/foben/schematizer/parse/BenchmarkingHandler.java
+++ src/main/java/net/foben/schematizer/parse/BenchmarkingHandler.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.openrdf.model.Statement;
-import org.openrdf.rio.RDFHandlerException;
 
 public class BenchmarkingHandler extends AbstractHandler {
 	
